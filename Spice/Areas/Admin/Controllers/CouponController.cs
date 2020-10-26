af1baac1bdf9eb801454529b795f50b7248d02e1
Tony5768 <tonkatazl@gmail.com>
2020-10-26 16:08:30 +0200

    Finished Coupon Functionality

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -54,6 +55,105 @@ namespace Spice.Areas.Admin.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(coupons);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            if (coupon==null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit( Coupon coupons)
+        {
+            if (coupons.Id==0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await db.Coupon.Where(c => c.Id == coupons.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponFromDb.Picture = p1;
+                }
+                couponFromDb.MinimumAmount = coupons.MinimumAmount;
+                couponFromDb.Name = coupons.Name;
+                couponFromDb.Discount = coupons.Discount;
+                couponFromDb.CouponType = coupons.CouponType;
+                couponFromDb.IsActive = coupons.IsActive;
+
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(coupons);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (coupon==null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coupon = await db.Coupon.SingleAsync(m => m.Id == id);
+            db.Coupon.Remove(coupon);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
