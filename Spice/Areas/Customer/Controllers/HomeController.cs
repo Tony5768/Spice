95d81c85b8428b6630d9922cba95a1f2eb98eff0
Tony5768 <tonkatazl@gmail.com>
2020-10-26 21:06:47 +0200

    Designed Home Page

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -4,24 +4,36 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Spice.Data;
 using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              ApplicationDbContext db)
         {
-            _logger = logger;
+            this.db = db;
+            this.logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel indexVM = new IndexViewModel()
+            {
+                MenuItems = await db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Categories = await db.Category.ToListAsync(),
+                Coupons = await db.Coupon.Where(m => m.IsActive == true).ToListAsync()
+            };
+            return View(indexVM);
         }
 
         public IActionResult Privacy()
